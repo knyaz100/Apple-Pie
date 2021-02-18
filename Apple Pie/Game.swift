77c26f2a924a300dc4b5ab543afb038edd3f67b6
Vasily Churbanov <vasily.churbanov@gmail.com>
2021-02-18 21:42:36 +0300

    Added Model

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Vasily Churbanov on 2021-02-18.
+//
+
+struct Game {
+    
+    var word: String
+    var incorrectMovesRemaining: Int
+    
+}
