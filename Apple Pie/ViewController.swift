337aab900a265d22cbae1ae9e42732fb1beb365e
Vasily Churbanov <vasily.churbanov@gmail.com>
2021-02-18 19:09:37 +0300

    Initial Commit

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Vasily Churbanov on 2021-02-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
